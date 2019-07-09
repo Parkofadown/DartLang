1e75ec92d80a053991cc2a7f86cc5d36f8ed766a
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-07-09 08:33:45 -0400

    Add files via upload

---
+++ constvariables.dart
@@ -0,0 +1,11 @@
+void main() {
+
+   final v1 = 9; // final can only be set once
+   const v2 = 10; // const cannot be re-assigned
+
+   //v1 = 8;
+   //v2 = 2;   
+
+   print(v1);
+   print(v2);
+}
