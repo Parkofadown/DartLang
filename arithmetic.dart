83d7237d49352b05230727fb849cf747c7ec8845
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-07-12 07:52:44 -0400

    Add files via upload

---
+++ arithmetic.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  int num1 = 10;
+  int num2 = 5;  
+ // add
+  print(num1 + num2);
+ // subtract
+  print(num1 - num2);
+ // multiply
+  print(num1 * num2);
+ // division
+  print(num1 / num2);
+ // unary minus
+  print(-num1);
+ // modulus
+  print(num1 % num2);
+ // increment 
+  num1++;
+  // decrement
+  num1--;
+}
