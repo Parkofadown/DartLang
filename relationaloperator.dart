6dc4f2b5fd88a5e27beb78f8df68c8d99ad76c64
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-07-12 08:03:56 -0400

    Add files via upload

---
+++ relationaloperator.dart
@@ -0,0 +1,24 @@
+void main()
+{
+ 
+  int num1 = 10;
+  int num2 = 5;
+
+  //greater than >
+  print(num1 > num2);
+
+  //less than < 
+  print(num1 < num2);
+ 
+  // greater than or equal
+  print(num1 >= num2);
+
+  // Less than or equal to
+  print(num1 <= num2);
+
+  // equal to ==
+  print(num1 == num2);
+
+  //not equal !=
+  print(num1 != num2);
+}
